f665ecf1836c8ca62f07a422e40ac74ab907672e
Amrita <amrita@Amritas-Laptop.bbrouter>
2025-01-28 15:34:44 +0530

    Kafka implemntation

---
+++ src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    kafka: {
+      TOPIC: 'test',
+      BROKERS: ['localhost:9092'],
+      GROUPID: 'bills-consumer-group',
+      CLIENTID: 'sample-kafka-client'
+      }
+   }
+   
+   
